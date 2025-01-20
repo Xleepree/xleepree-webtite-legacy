8ef85059b324e8ba6730dc466dc93b06968c40e4
Xleepree <diegoed1313@gmail.com>
2025-01-20 14:06:18 -0500

    slocalglstorage
    fduid

--- js/theme-handler.js
+++ js/theme-handler.js
@@ -1,36 +1,36 @@
 function setLightThemeIndex() {
     document.getElementById('stylesheet').href='css/global.css';
-    sessionStorage.setItem("theme", "light");
+    localStorage.setItem("theme", "light");
 }
 
 function setDarkThemeIndex() {
     document.getElementById('stylesheet').href='css/global-dark.css';
-    sessionStorage.setItem("theme", "dark");
+    localStorage.setItem("theme", "dark");
 }
 
 function setHiCfThemeIndex() {
     document.getElementById('stylesheet').href='css/global-hi-cf.css';
-    sessionStorage.setItem("theme", "hicf");
+    localStorage.setItem("theme", "hicf");
 }
 
 function setSunriseThemeIndex() {
     document.getElementById('stylesheet').href='css/global-sunrise.css';
-    sessionStorage.setItem("theme", "sunrise");
+    localStorage.setItem("theme", "sunrise");
 }
 
 function setSolarizedLightThemeIndex() {
     document.getElementById('stylesheet').href='css/global-solarized-light.css';
-    sessionStorage.setItem("theme", "solarized-light");
+    localStorage.setItem("theme", "solarized-light");
 }
 
 function setSolarizedDarkThemeIndex() {
     document.getElementById('stylesheet').href='css/global-solarized-dark.css';
-    sessionStorage.setItem("theme", "solarized-dark");
+    localStorage.setItem("theme", "solarized-dark");
 }
 
 
 function checkThemeIndex() {
-    var storedValue = sessionStorage.getItem('theme'); 
+    var storedValue = localStorage.getItem('theme'); 
     if (storedValue === 'light') { setLightThemeIndex();};
     if (storedValue === 'dark') { setDarkThemeIndex();};
     if (storedValue === 'hicf') { setHiCfThemeIndex();};
@@ -40,7 +40,7 @@ function checkThemeIndex() {
 }
 
 function checkThemePages() {
-    var storedValue = sessionStorage.getItem('theme'); 
+    var storedValue = localStorage.getItem('theme'); 
     if (storedValue === 'light') { setLightThemePages();};
     if (storedValue === 'dark') { setDarkThemePages();};
     if (storedValue === 'hicf') { setHiCfThemePages();};
@@ -50,7 +50,7 @@ function checkThemePages() {
 }
 
 function checkThemeBlogPages() {
-    var storedValue = sessionStorage.getItem('theme'); 
+    var storedValue = localStorage.getItem('theme'); 
     if (storedValue === 'light') { setLightThemeBlogPages();};
     if (storedValue === 'dark') { setDarkThemeBlogPages();};
     if (storedValue === 'hicf') { setHiCfThemeBlogPages();};
@@ -62,63 +62,63 @@ function checkThemeBlogPages() {
 
 function setLightThemePages() {
     document.getElementById('stylesheet').href='../css/global.css';
-    sessionStorage.setItem("theme", "light");
+    localStorage.setItem("theme", "light");
 }
 
 function setDarkThemePages() {
     document.getElementById('stylesheet').href='../css/global-dark.css';
-    sessionStorage.setItem("theme", "dark");
+    localStorage.setItem("theme", "dark");
 }
 
 function setHiCfThemePages() {
     document.getElementById('stylesheet').href='../css/global-hi-cf.css';
-    sessionStorage.setItem("theme", "hicf");
+    localStorage.setItem("theme", "hicf");
 }
 
 function setSunriseThemePages() {
     document.getElementById('stylesheet').href='../css/global-sunrise.css';
-    sessionStorage.setItem("theme", "sunrise");
+    localStorage.setItem("theme", "sunrise");
 }
 
 function setSolarizedLightThemePages() {
     document.getElementById('stylesheet').href='../css/global-solarized-light.css';
-    sessionStorage.setItem("theme", "solarized-light");
+    localStorage.setItem("theme", "solarized-light");
 }
 
 function setSolarizedDarkThemePages() {
     document.getElementById('stylesheet').href='../css/global-solarized-dark.css';
-    sessionStorage.setItem("theme", "solarized-dark");
+    localStorage.setItem("theme", "solarized-dark");
 }
 
 
 function setLightThemeBlogPages() {
     document.getElementById('stylesheet').href='../../css/global.css';
-    sessionStorage.setItem("theme", "light");
+    localStorage.setItem("theme", "light");
 }
 
 function setDarkThemeBlogPages() {
     document.getElementById('stylesheet').href='../../css/global-dark.css';
-    sessionStorage.setItem("theme", "dark");
+    localStorage.setItem("theme", "dark");
 }
 
 function setHiCfThemeBlogPages() {
     document.getElementById('stylesheet').href='../../css/global-hi-cf.css';
-    sessionStorage.setItem("theme", "hicf");
+    localStorage.setItem("theme", "hicf");
 }
 
 function setSunriseThemeBlogPages() {
     document.getElementById('stylesheet').href='../../css/global-sunrise.css';
-    sessionStorage.setItem("theme", "sunrise");
+    localStorage.setItem("theme", "sunrise");
 }
 
 function setSolarizedLightThemeBlogPages() {
     document.getElementById('stylesheet').href='../../css/global-solarized-light.css';
-    sessionStorage.setItem("theme", "solarized-light");
+    localStorage.setItem("theme", "solarized-light");
 }
 
 function setSolarizedDarkThemeBlogPages() {
     document.getElementById('stylesheet').href='../../css/global-solarized-dark.css';
-    sessionStorage.setItem("theme", "solarized-dark");
+    localStorage.setItem("theme", "solarized-dark");
 }
 
 
