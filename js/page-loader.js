094dbb857373a23875433f4d447f56e5143c3faa
Xleepree <diegoed1313@gmail.com>
2025-01-22 21:58:42 -0500

    pagelodaderws
    yeaaaaaaaaaaaaboi

---
+++ js/page-loader.js
@@ -0,0 +1,49 @@
+function loadFooterIndex() {
+    document.getElementById("footer").innerHTML = 
+    "<button onclick='setLightThemeIndex()'>light</button> <button onclick='setDarkThemeIndex()'>dark</button> <button onclick='setHiCfThemeIndex()'>high contrast</button> <button onclick='setSunriseThemeIndex()'>sunrise</button> <button onclick='setSolarizedLightThemeIndex()'>solarized light</button> <button onclick='setSolarizedDarkThemeIndex()'>solarized dark</button> <hr> <p><small>since nov 3, 2024 :: hosted with cloudflare </small></p> <a href='https://github.com/Xleepree/xleepree_webtite'>repository</a> | <a href='mailto:xleepree.pages.mail@gmail.com'>contact</a>";
+}
+
+function loadNavIndex() {
+    document.getElementById("nav").innerHTML = 
+    "<a href='index.html'>home</a> <a href='pages/blog.html'>blog</a> <a href='pages/projects.html'>projects</a> <a href='pages/changelog.html'>changelog</a> <a href='pages/about.html'>about</a>"
+}
+
+function loadBannerIndex() {
+    document.getElementById("banner").innerHTML = 
+    "<img style='object-fit: contain; max-width: 55em; border-radius: 15px; width: 100%; height: 100%; box-shadow: 0px 0.5em 10px;' src='images/banner.png'>"
+}
+
+//pages VV
+
+function loadFooterPages() {
+    document.getElementById("footer").innerHTML = 
+    "<button onclick='setLightThemePages()'>light</button> <button onclick='setDarkThemePages()'>dark</button> <button onclick='setHiCfThemePages()'>high contrast</button> <button onclick='setSunriseThemePages()'>sunrise</button> <button onclick='setSolarizedLightThemePages()'>solarized light</button> <button onclick='setSolarizedDarkThemePages()'>solarized dark</button> <hr> <p><small>since nov 3, 2024 :: hosted with cloudflare </small></p> <a href='https://github.com/Xleepree/xleepree_webtite'>repository</a> | <a href='mailto:xleepree.pages.mail@gmail.com'>contact</a>"
+}
+
+function loadBannerPages() {
+    document.getElementById("banner").innerHTML = 
+    "<img style='object-fit: contain; max-width: 55em; border-radius: 15px; box-shadow: 0px 0.5em 10px; width: 100%; height: 100%;' src='../images/banner.png'>"
+}
+
+function loadNavPages() {
+    document.getElementById("nav").innerHTML = 
+    "<a href='../index.html'>home</a> <a href='blog.html'>blog</a> <a href='projects.html'>projects</a> <a href='changelog.html'>changelog</a> <a href='about.html'>about</a>"
+}
+
+//blog pages VV
+
+function loadNavBlogPages() {
+    document.getElementById("nav").innerHTML = 
+    "<a href='../../index.html'>home</a> <a href='../blog.html'>blog</a> <a href='../projects.html'>projects</a> <a href='../changelog.html'>changelog</a> <a href='../about.html'>about</a>"
+}
+
+function loadBannerBlogPages() {
+    document.getElementById("banner").innerHTML = 
+    "<img style='object-fit: contain; max-width: 55em; border-radius: 15px; box-shadow: 0px 0.5em 10px; width: 100%; height: 100%;' src='../../images/banner.png'>"
+}
+
+function loadFooterBlogPages() {
+    document.getElementById("footer").innerHTML = 
+    "<button onclick='setLightThemeBlogPages()'>light</button> <button onclick='setDarkThemeBlogPages()'>dark</button> <button onclick='setHiCfThemeBlogPages()'>high contrast</button> <button onclick='setSunriseThemeBlogPages()'>sunrise</button> <button onclick='setSolarizedLightThemeBlogPages()'>solarized light</button> <button onclick='setSolarizedDarkThemeBlogPages()'>solarized dark</button> <hr> <p><small>since nov 3, 2024 :: hosted with cloudflare </small></p> <a href='https://github.com/Xleepree/xleepree_webtite'>repository</a> | <a href='mailto:xleepree.pages.mail@gmail.com'>contact</a>"
+}
+
